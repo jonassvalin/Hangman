f6c7b9e27a3c6d2ef80f726a252ce91b646f44db
jonassvalin <jonassvalin@gmail.com>
2015-08-13 02:24:02 +0200

    Code cleanup, added comments and added CSS visuals

--- Hangman/index.php
+++ Hangman/index.php
@@ -1,20 +1,16 @@
-<html>
-<head>
-<title>Duke Hangem' 3D</title>
-<link href="style.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
-<h1>Duke Hangem' 3D</h1>
-<img src="images/duke.jpg" height="250" width="250"><br>
-<form action="initialize.php" method="POST">
-	Difficulty: 
-	<select name="difficulty">
-		<option value="easy">Piece Of Cake</option>
-		<option value="medium">Let's Rock</option>
-		<option value="hard">Come Get Some</option>
-	</select><br>
-	Start New Game:
-	<input type="submit" name="newgame" value="Go!">
-</form>
-</body>
-</html>
+<?php
+include 'graphics.php';
+
+echo getIntroHtml();
+echo '<img src="images/duke.jpg" height="250" width="250"><br><br>
+			<form action="initialize.php" method="POST">
+				Difficulty:
+				<select name="difficulty">
+					<option value="easy">Piece Of Cake</option>
+					<option value="medium">Let\'s Rock</option>
+					<option value="hard">Come Get Some</option>
+				</select><br><br>
+				Start New Game:
+				<input type="submit" name="newgame" value="Go!">';
+echo getOutroHtml();
+?>
--- Hangman/update.php
+++ Hangman/update.php
@@ -1,16 +1,23 @@
 <?php session_start();
+include 'graphics.php';
 
+/**
+* Main operations
+*/
 $GLOBALS['alphabet'] = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
 		"l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z");
 $guess = $_POST["guess"];
 $word = $_SESSION["word"];
 $wordLength = strlen($word);
 $failedAttempts = $_SESSION["failed"];
-
 if (!checkGuess($guess, $word, $wordLength)) $failedAttempts++;
 checkStatus($failedAttempts, $word, $wordLength);
 updateGraphics($failedAttempts, $word, $wordLength);
 
+/**
+* Checks whether the guessed character is contained within the word.
+* Returns true if it was, otherwise false.
+*/
 function checkGuess($guess, $word, $wordLength) {
 	$_SESSION[$guess] = "used";
 	$correctGuess = false;
@@ -23,24 +30,32 @@ function checkGuess($guess, $word, $wordLength) {
 	return $correctGuess;
 }
 
+/**
+* Checks if the player has either won or lost the game. If either is the case
+* the game is ended.
+*/
 function checkStatus($failedAttempts, $word, $wordLength) {
 	$allCorrect = true;
 	for($i = 0; $i < $wordLength; $i++) {
 		$char = $_SESSION["char".$i];
 		if($char == "hidden") $allCorrect = false;
 	}
-	
+
 	if($allCorrect) {
-		restartGame("You Win!", $word, $failedAttempts);
+		finishGame("You Win!", $word, $failedAttempts);
 	} else if ($failedAttempts > 9) {
-		restartGame("You Lose!", $word, $failedAttempts);
+		finishGame("You Lose!", $word, $failedAttempts);
 	}
-	
+
 	$_SESSION["failed"] = $failedAttempts;
 }
 
+/**
+* Updates the visuals to represent the current state of the game.
+*/
 function updateGraphics($failedAttempts, $word, $wordLength) {
-	echo '<img src="images/hang' . $failedAttempts . '.gif" height="150" width="150"><br>';
+	echo getIntroHtml();
+	echo '<img src="images/hang' . $failedAttempts . '.gif" height="190" width="190"><br>';
 	for($i = 0; $i < $wordLength; $i++) {
 		$char = $_SESSION["char".$i];
 		if($char == "hidden") echo "_ ";
@@ -48,24 +63,31 @@ function updateGraphics($failedAttempts, $word, $wordLength) {
 	}
 	echo '
 		<form action="update.php" method="POST"><br>
-		Guess: <input type="text" name="guess" maxlength="1"><br>
+		Guess: <input type="text" name="guess" maxlength="1"><br><br>
 		<input type="submit" name="update" value="Submit Guess!"><br><br>
-			Used characters:  
+			Used characters:
 		';
 	$alphabet = $GLOBALS['alphabet'];
 	foreach ($alphabet as $letter) {
 		if($_SESSION[$letter] == "used") echo $letter . " ";
 	}
+	echo getOutroHtml();
 }
 
-function restartGame($output, $word, $failedAttempts) {
-	echo '<img src="images/hang' . $failedAttempts . '.gif" height="150" width="150"><br>';
+/**
+* Finishes the game, displays the correct word and inquiries the player if
+* he/she would like to play again.
+*/
+function finishGame($output, $word, $failedAttempts) {
+	echo getIntroHtml();
+	echo '<img src="images/hang' . $failedAttempts . '.gif" height="190" width="190"><br><br>';
 	session_destroy();
-	echo $output . " The correct answer was: " . $word;
+	echo $output . ' The correct answer was: <b>' . $word . '</b>';
 	echo '
 		<form action="index.php" method="POST"><br>
-		<input type="submit" name="restart" value="Restart Game?">
+		<input type="submit" name="restart" value="Restart Game?"><br><br><br><br>
 		';
+	echo getOutroHtml();
 	exit();
 }
 
